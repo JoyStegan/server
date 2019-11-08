6dc33a080fb71976ee7fe4f0e4cdbbc4e9fb78e6
uzaisindiko <uzai.sindiko@gmail.com>
2019-11-08 13:03:22 +0700

    make find

--- routes/translationRoute.js
+++ routes/translationRoute.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const TranslationController = require('../controllers/TranslationController')
 
 router.post('/', TranslationController.translating)
+router.get('/', TranslationController.find)
 
 module.exports = router
