b8578ee12ba1bb3518456569e991ddfd370c1ae2
straf16 <depoksugutamu621@gmail.com>
2019-11-08 11:26:09 +0700

    gcs upload work with translation api

--- routes/translationRoute.js
+++ routes/translationRoute.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
+const { upload } = require('../middlewares/gcsUpload')
 const TranslationController = require('../controllers/TranslationController')
 
-router.post('/', TranslationController.translating)
+router.post('/', upload.single('url'), TranslationController.translating)
 
 module.exports = router
